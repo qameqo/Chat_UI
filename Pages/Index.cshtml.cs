55ed1776457735dab2c48cfb463aa79b7b6cd3b6
qameqo <qameqo.98@gmail.com>
2024-12-27 13:27:02 +0700

    [EDIT] : add url websocket to appsetting

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,15 +6,18 @@ namespace Websocket_UI.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly IConfiguration _configuration;
+        [BindProperty]
+        public string URL { get; set; }
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
-
+           URL = _configuration["URL_SOCKET"];
         }
     }
 }

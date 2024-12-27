7c970a56104dd3e5a02a41168bb4d80b9edd4d1a
qameqo <qameqo.98@gmail.com>
2024-12-27 21:09:27 +0700

    [EDIT]: set option prod

--- Program.cs
+++ Program.cs
@@ -12,13 +12,14 @@ var options = new ConfigurationOptions
     Ssl = true,
     User = redisConfig["username"]
 };
-//if (builder.Environment.IsProduction())
-//{
-//    options = new ConfigurationOptions
-//    {
-//        EndPoints = { EndPoint },
-//    };
-//}
+if (builder.Environment.IsProduction())
+{
+    EndPoint = "redis://red-ctn864dds78s73c22v0g:6379";
+    options = new ConfigurationOptions
+    {
+        EndPoints = { EndPoint },
+    };
+}
 
 builder.Services.AddSingleton<RedisService>(sp =>
     new RedisService(options));
